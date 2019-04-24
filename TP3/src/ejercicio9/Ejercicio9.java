7f490135d062058841795ab9ca59d8c0445cfb9c
franco palermo <franco.palermo812@gmail.com>
2019-04-23 23:53:55 -0300

    Modificar Ejercicio 9 del TP 3 para que tome un Array con las letras que se desean combinar.

--- TP3/src/ejercicio9/Ejercicio9.java
+++ TP3/src/ejercicio9/Ejercicio9.java
@@ -4,21 +4,22 @@ public class Ejercicio9 {
 
     private static int index = 0;
 
-    public static String[] combinar(int k) {
+    public static String[] combinar(String[] letras, int k) {
 
         if ( k < 0 ) throw new IndexOutOfBoundsException();
         if ( k == 0 ) return new String[0];
+        if ( letras.length == 0 ) throw new IllegalStateException();
 
         index = 0;
 
-        String[] combinaciones = new String[(int)Math.pow(2, k)];
+        String[] combinaciones = new String[(int)Math.pow(letras.length, k)];
 
-        combinarRecursivo("", k, combinaciones);
+        combinarRecursivo(letras,"", k, combinaciones);
 
         return combinaciones;
     }
 
-    public static void combinarRecursivo(String actual, int k, String[] combinaciones) {
+    public static void combinarRecursivo(String[] letras, String actual, int k, String[] combinaciones) {
 
         if(actual.length() == k) {
             System.out.println(actual);
@@ -26,8 +27,11 @@ public class Ejercicio9 {
         }
         else {
             // TODO: CAMBIAR POR UN FOR
-            combinarRecursivo(actual + "N", k, combinaciones);
-            combinarRecursivo(actual + "R", k, combinaciones);
+            for (int i = 0; i < letras.length; i++) {
+                combinarRecursivo(letras, actual + letras[i], k, combinaciones);
+            }
+            //combinarRecursivo(letras,actual + "N", k, combinaciones);
+            //combinarRecursivo(letras,actual + "R", k, combinaciones);
         }
     }
 }
--- TP3/test/ejercicio9/Ejercicio9Test.java
+++ TP3/test/ejercicio9/Ejercicio9Test.java
@@ -1,5 +1,6 @@
 package ejercicio9;
 
+import ejercicio6.Ejercicio6;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,27 +10,40 @@ public class Ejercicio9Test {
 
     @Test(expected =  IndexOutOfBoundsException.class)
     public void combinar_conArgumentoNegativo_LanzaError() {
+        // ARRANGE
+        int k = -1;
+        String[] letras = new String[] {"B", "N"};
 
         // ACT
-        Ejercicio9.combinar(-1);
+        Ejercicio9.combinar(letras, k);
+    }
+
+    @Test(expected =  IllegalStateException.class)
+    public void combinar_KDistintoDeCeroYArrayConLetras_LanzaError() {
+        // ARRANGE
+        int k = 3;
+        String[] letras = new String[0];
+
+        // ACT
+        Ejercicio9.combinar(letras, k);
     }
 
     @Test
-    public void combinar_conCero_DevuelveArrayVacio() {
-
+    public void combinar_KIgualCero_DevuelveArrayVacio() {
         // ARRANGE
         int k = 0;
+        String[] letras = new String[] {"R", "N"};
         String[] expected = new String[0];
 
         // ACT
-        String[] result = Ejercicio9.combinar(k);
+        String[] result = Ejercicio9.combinar(letras, k);
 
         // ASSERT
         Assert.assertEquals(expected.length, result.length);
         Assert.assertArrayEquals(expected, result);
     }
 
-    @Test
+    /*@Test
     public void combinar_conArgumentoIgualAUno_DevuelveDosCombinaciones() {
 
         // ARRANGE
@@ -42,9 +56,9 @@ public class Ejercicio9Test {
         // ASSERT
         Assert.assertEquals(expected.length, result.length);
         Assert.assertArrayEquals(expected, result);
-    }
+    }*/
 
-    @Test
+    /*@Test
     public void combinar_conArgumentoIgualADos_DevuelveTresCombinaciones() {
 
         // ARRANGE
@@ -57,9 +71,9 @@ public class Ejercicio9Test {
         // ASSERT
         Assert.assertEquals(expected.length, result.length);
         Assert.assertArrayEquals(expected, result);
-    }
+    }*/
 
-    @Test
+    /*@Test
     public void combinar_conArgumentoIgualATres_DevuelveOchoCombinaciones() {
 
         // ARRANGE
@@ -72,5 +86,93 @@ public class Ejercicio9Test {
         // ASSERT
         Assert.assertEquals(expected.length, result.length);
         Assert.assertArrayEquals(expected, result);
+    }*/
+
+    @Test
+    public void combinar_KIgual2YDosLetras_Ok() {
+        // ARRANGE
+        int k = 2;
+
+        String[] letras = new String[] {
+                "R", "N"
+        };
+
+        String[] expected = new String[] {
+                "RR", "RN", "NR", "NN"
+        };
+
+        // ACT
+        String[] resultado = Ejercicio9.combinar(letras, k);
+
+        // ASSERT
+        Assert.assertEquals(expected.length, resultado.length);
+        Assert.assertArrayEquals(expected, resultado);
+    }
+
+    @Test
+    public void combinar_KIgual3YDosLetras_Ok() {
+        // ARRANGE
+        int k = 3;
+
+        String[] letras = new String[] {
+                "R", "N"
+        };
+
+        String[] expected = new String[] {
+                "RRR", "RRN", "RNR", "RNN", "NRR", "NRN", "NNR", "NNN"
+        };
+
+        // ACT
+        String[] resultado = Ejercicio9.combinar(letras, k);
+
+        // ASSERT
+        Assert.assertEquals(expected.length, resultado.length);
+        Assert.assertArrayEquals(expected, resultado);
+    }
+
+    @Test
+    public void combinar_KIgual3YTresLetras_Ok() {
+        // ARRANGE
+        int k = 3;
+
+        String[] letras = new String[] {
+                "R", "N", "S"
+        };
+
+        String[] expected = new String[] {
+                "RRR", "RRN", "RRS", "RNR", "RNN", "RNS", "RSR", "RSN", "RSS",
+                "NRR", "NRN", "NRS", "NNR", "NNN", "NNS", "NSR", "NSN", "NSS",
+                "SRR", "SRN", "SRS", "SNR", "SNN", "SNS", "SSR", "SSN", "SSS"
+        };
+
+        // ACT
+        String[] resultado = Ejercicio9.combinar(letras, k);
+
+        // ASSERT
+        Assert.assertEquals(expected.length, resultado.length);
+        Assert.assertArrayEquals(expected, resultado);
+    }
+
+    @Test
+    public void combinar_KIgual2YTresLetras_Ok() {
+        // ARRANGE
+        int k = 2;
+
+        String[] letras = new String[] {
+                "R", "N", "S"
+        };
+
+        String[] expected = new String[] {
+                "RR", "RN", "RS",
+                "NR", "NN", "NS",
+                "SR", "SN", "SS"
+        };
+
+        // ACT
+        String[] resultado = Ejercicio9.combinar(letras, k);
+
+        // ASSERT
+        Assert.assertEquals(expected.length, resultado.length);
+        Assert.assertArrayEquals(expected, resultado);
     }
 }
