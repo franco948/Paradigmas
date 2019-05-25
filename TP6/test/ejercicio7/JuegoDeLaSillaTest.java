bba23522ee7203b50ac25123e6e085daaa50f659
franco948 <franco.palermo812@gmail.com>
2019-05-24 22:50:52 -0300

    Agregar mas tests a juego de la silla.

--- TP6/test/ejercicio7/JuegoDeLaSillaTest.java
+++ TP6/test/ejercicio7/JuegoDeLaSillaTest.java
@@ -4,6 +4,7 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -37,9 +38,39 @@ public class JuegoDeLaSillaTest {
         Assert.assertEquals( jugadores.get(0), ganadorResultado );
     }
 
-    @Test
-    public void nuevaVuelta() {
+    @Test(expected = IllegalStateException.class)
+    public void jugar_MenosDeDosJugadores_Error() throws Exception {
+        // ARRANGE
+        List<Jugador> jugadores = new LinkedList<>();
+        jugadores.add(new Jugador("Dario"));
+
+        JuegoDeLaSilla juegoDeLaSilla = new JuegoDeLaSilla(1);
+
+
+        // ACT
+        juegoDeLaSilla.jugar(jugadores);
     }
+/*
+    @Test
+    public void nuevaVuelta() throws Exception {
+        // ARRANGE
+        List<Jugador> jugadores = new LinkedList<>();
+        jugadores.add(new Jugador("Dario"));
+        jugadores.add(new Jugador("Hernan"));
+        jugadores.add(new Jugador("Alejandro"));
+        jugadores.add(new Jugador("Julian"));
+        jugadores.add(new Jugador("Mariana"));
+
+        JuegoDeLaSilla juegoDeLaSilla = new JuegoDeLaSilla(1);
+
+        // ACT
+        Jugador perdedorResultado = juegoDeLaSilla.nuevaVuelta();
+
+        // ASSERT
+        Assert.assertTrue( jugadores.size() == 4 );
+
+        Assert.assertFalse( jugadores.contains(perdedorResultado) );
+    }*/
 
     @Test
     public void imprimir() {
